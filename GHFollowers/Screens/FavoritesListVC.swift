3ca18905320ccf171131664455f173e4d71f8625
Christyan Duarte <duarteich@gmail.com>
2023-03-18 08:35:20 -0600

    Initial commit: Search followers UI created

---
+++ GHFollowers/Screens/FavoritesListVC.swift
@@ -0,0 +1,16 @@
+//
+//  FavoritesListVC.swift
+//  GHFollowers
+//
+//  Created by Christyan Duarte on 20/02/23.
+//
+
+import UIKit
+
+class FavoritesListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBlue
+    }
+}
