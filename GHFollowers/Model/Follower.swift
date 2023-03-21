055f1f74b7ccf5ef9bdf3108ca7eb28196337f08
Christyan Duarte <duarteich@gmail.com>
2023-03-21 15:08:45 -0600

    - Add Follower and User models - Quick bug fix

---
+++ GHFollowers/Model/Follower.swift
@@ -0,0 +1,13 @@
+//
+//  Follower.swift
+//  GHFollowers
+//
+//  Created by Christyan Duarte on 18/03/23.
+//
+
+import Foundation
+
+struct Follower: Codable {
+    var login: String
+    var avatarUrl: String
+}
