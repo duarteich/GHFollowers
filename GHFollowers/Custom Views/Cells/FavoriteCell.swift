bb8f28f680331c5a2b73eb06c46577c920b4c378
Christyan Duarte <duarteich@gmail.com>
2023-06-12 14:25:20 -0600

    Clean Up and Optimization 1 and 2

--- GHFollowers/Custom Views/Cells/FavoriteCell.swift
+++ GHFollowers/Custom Views/Cells/FavoriteCell.swift
@@ -10,12 +10,12 @@ import UIKit
 class FavoriteCell: UITableViewCell {
 
     static let reuseID = "FavoriteCell"
-    
     let avatarImageView = GFAvatarImageView(frame: .zero)
     let usernameLabel = GFTitleLabel(textAlignment: .left, fontSize: 26)
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
+        configure()
     }
     
     required init?(coder: NSCoder) {
@@ -24,7 +24,10 @@ class FavoriteCell: UITableViewCell {
     
     func set(favorite: Follower) {
         usernameLabel.text = favorite.login
-        avatarImageView.downloadImage(from: favorite.avatarUrl)
+        NetworkManager.shared.downloadImage(from: favorite.avatarUrl) { [weak self] image in
+            guard let self = self else { return }
+            DispatchQueue.main.async { self.avatarImageView.image = image }
+        }
     }
     
     private func configure() {
@@ -43,7 +46,7 @@ class FavoriteCell: UITableViewCell {
             usernameLabel.centerYAnchor.constraint(equalTo: self.centerYAnchor),
             usernameLabel.leadingAnchor.constraint(equalTo: avatarImageView.trailingAnchor, constant: 24),
             usernameLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -padding),
-            usernameLabel.heightAnchor.constraint(equalToConstant: 40)
+            usernameLabel.heightAnchor.constraint(equalToConstant: 40),
         ])
     }
 }
